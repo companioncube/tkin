88803f415155b41f208a8fdf55f53c6743a1c36c
Farhan <farhanqazifq@gmail.com>
2022-07-29 20:34:38 +0530

    f

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ myLabel1 = Label(root, text="Hello World!")
 myLabel2 = Label(root, text="Bye World!")
 
 myLabel1.grid(row=0, column=0)
-myLabel1.grid(row=1, column=0)
+myLabel2.grid(row=1, column=1)
 
 
 
