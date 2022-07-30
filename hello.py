7be5781c340e071dfb211fac5c0c6d9987523870
Farhan <farhanqazifq@gmail.com>
2022-07-30 09:52:06 +0530

    sf

--- hello.py
+++ hello.py
@@ -2,15 +2,21 @@ from tkinter import *
 
 root = Tk()
 
-myLabel1 = Label(root, text="Hello World!")
+e=Entry(root, width=50)
+
+def myClick():
+    myLabel1 = Label(root, text="EHHH")
+    myLabel1.grid(row=0, column=0)
+
 myLabel2 = Label(root, text="Bye World!")
 myLabel3 = Label(root, text="B World!")
-myButton = Button(root, text="Click me",state=DISABLED,padx=50)
+myButton = Button(root, text="Click me",padx=50,pady=50,command=myClick,fg="red",bg="green")
 
 myButton.grid(row=2, column=0)
-myLabel1.grid(row=0, column=0)
+
 myLabel2.grid(row=1, column=1)
 myLabel3.grid(row=1, column=2)
+e.grid(row=2, column=1)
 
 
 
